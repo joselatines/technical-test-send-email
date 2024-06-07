6fbc54b01b32956e22bb885a121ad2a169efc734
Jose Latines <joselatines33@gmail.com>
2024-06-07 15:13:01 -0400

    feat: add upload folder

--- src/utils/multer.js
+++ src/utils/multer.js
@@ -3,7 +3,7 @@ import path from "path";
 
 const storage = multer.diskStorage({
 	destination: (req, file, cb) => {
-		cb(null, "./temp");
+		cb(null, "./uploads");
 	},
 	filename: (req, file, cb) => {
 		cb(null, file.originalname.replace("", "-"));
